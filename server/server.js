35e5980669f2c2432e57685c0bf43aa66c6dd8a7
Mike M <mikal815@users.noreply.github.com>
2018-12-17 23:58:49 -0500

    Add prodcut and wood models/save and find products and woods

--- server/server.js
+++ server/server.js
@@ -16,7 +16,8 @@ app.use(cookieParser());
 // Models
 const { User } = require('./models/user');
 const { Brand } = require('./models/brand');
-
+const { Wood } = require('./models/wood');
+const { Product } = require('./models/product');
 
 // Middlewares
 const { auth } = require('./middleware/auth');
@@ -24,7 +25,94 @@ const { admin } = require('./middleware/admin');
 
 
 //=================================
-//              Brand
+//             PRODUCTS
+//=================================
+
+// BY ARRIVAL
+// /articles?sortBy=createdAt&order=desc&limit=4
+
+// BY SELL
+// /articles?sortBy=sold&order=desc&limit=100&skip=5
+app.get('/api/product/articles',(req,res)=>{
+
+    let order = req.query.order ? req.query.order : 'asc';
+    let sortBy = req.query.sortBy ? req.query.sortBy : "_id";
+    let limit = req.query.limit ? parseInt(req.query.limit) : 100;
+
+    Product.
+    find().
+    populate('brand').
+    populate('wood').
+    sort([[sortBy,order]]).
+    limit(limit).
+    exec((err,articles)=>{
+        if(err) return res.status(400).send(err);
+        res.send(articles)
+    })
+})
+
+
+/// /api/product/article?id=HSHSHSKSK,JSJSJSJS,SDSDHHSHDS,JSJJSDJ&type=single
+app.get('/api/product/articles_by_id',(req,res)=>{
+    let type = req.query.type;
+    let items = req.query.id;
+
+    if(type === "array"){
+        let ids = req.query.id.split(',');
+        items = [];
+        items = ids.map(item=>{
+            return mongoose.Types.ObjectId(item)
+        })
+    }
+
+    Product.
+    find({ '_id':{$in:items}}).
+    populate('brand').
+    populate('wood').
+    exec((err,docs)=>{
+        return res.status(200).send(docs)
+    })
+});
+
+
+app.post('/api/product/article',auth,admin,(req,res)=>{
+    const product = new Product(req.body);
+
+    product.save((err,doc)=>{
+        if(err) return res.json({success:false,err});
+        res.status(200).json({
+            success: true,
+            article: doc
+        })
+    })
+})
+
+//=================================
+//              WOODS
+//=================================
+
+app.post('/api/product/wood',auth,admin,(req,res)=>{
+    const wood = new Wood(req.body);
+
+    wood.save((err,doc)=>{
+        if(err) return res.json({success:false,err});
+        res.status(200).json({
+            success: true,
+            wood: doc
+        })
+    })
+});
+
+app.get('/api/product/woods',(req,res)=>{
+    Wood.find({},(err,woods)=>{
+        if(err) return res.status(400).send(err);
+        res.status(200).send(woods)
+    })
+})
+
+
+//=================================
+//              BRAND
 //=================================
 
 app.post('/api/product/brand',auth,admin,(req,res)=>{
